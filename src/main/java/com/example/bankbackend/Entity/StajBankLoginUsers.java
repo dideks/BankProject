84bf37e1cc5c1894f485079a725cfc0d48fa44a7
dideks <didekose@icloud.com>
2024-07-22 15:01:20 +0300

    Modify StajBankLoginUsers.java

--- src/main/java/com/example/bankbackend/Entity/StajBankLoginUsers.java
+++ src/main/java/com/example/bankbackend/Entity/StajBankLoginUsers.java
@@ -1,9 +1,30 @@
 package com.example.bankbackend.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name=)
+@Table(name="STAJBANKLOGINUSERS")
 public class StajBankLoginUsers {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+
+    private Long id;
+
+    @Column(name="USERNAME")
+    private String username;
+
+    @Column(name="PASSWORD")
+    private String password;
+
+    public Long getId() {return id; }
+
+    public void setId(Long id) {this.id = id;}
+
+    public String getUsername() {return username;}
+
+    public void setUsername(String username) {this.username = username;}
+
+    public String getPassword() {return password;}
+
+    public void setPassword(String password) {this.password = password; }
 }
